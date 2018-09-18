69abd45193bc0e958362fca62d6b81875879c68a
Sandra <sandraa18@ru.is>
2018-09-18 11:45:34 +0000

    https://github.com/sandraa18/TileTraveller.git

--- implementation1.py
+++ implementation1.py
@@ -4,65 +4,65 @@
 #Finna út nýja gildi á start miðað við hvaða átt er slegin inn
 #Halda áfram í while lykkju þangað til við erum komin á reit 3.1, þá er búið að vinna
 
-start = 1.1
+start_x = 1
+start_y = 1
 
-while start != 3.1 :
-
-    if start == 1.1:
-        valid_direction = "n"
-        print("You can travel: (N)orth.")
-    elif start == 1.2:
-        valid_direction = "nes"
-        print("You can travel: (N)orth or (E)ast or (S)outh.")
-    elif start == 1.3:
-        valid_direction = "es"
-        print("You can travel: (E)ast or (S)outh.")
-    elif start == 2.1:
-        valid_direction = "n"
-        print("You can travel: (N)orth.")
-    elif start == 2.2:
-        valid_direction = "ws"
-        print("You can travel: (S)outh or (W)est.")
-    elif start == 2.3:
-        valid_direction = "ew"
-        print("You can travel: (E)ast or (W)est.")
-    elif start == 3.1:
-        valid_direction = "n"
-        print("You can travel: (N)orth.")
-    elif start == 3.2:
-        valid_direction = "ns"
-        print("You can travel: (N)orth or (S)outh.")
-    elif start == 3.3:
-        valid_direction = "ws"
-        print("You can travel: (S)outh or (W)est.")
+while start_x != 3 or start_y != 1 :
+    if start_x == 1:
+        if start_y == 1:
+            valid_direction = "n"
+            print("You can travel: (N)orth.")
+        elif start_y == 2:
+            valid_direction = "nes"
+            print("You can travel: (N)orth or (E)ast or (S)outh.")
+        elif start_y == 3:
+            valid_direction = "es"
+            print("You can travel: (E)ast or (S)outh.")
+    elif start_x == 2:
+        if start_y == 1:
+            valid_direction = "n"
+            print("You can travel: (N)orth.")
+        elif start_y == 2:
+            valid_direction = "ws"
+            print("You can travel: (S)outh or (W)est.")
+        elif start_y == 3:
+            valid_direction = "ew"
+            print("You can travel: (E)ast or (W)est.")
+    elif start_x == 3 :
+        if start_y == 1:
+            valid_direction = "n"
+            print("You can travel: (N)orth.")
+        elif start_y == 2:
+            valid_direction = "ns"
+            print("You can travel: (N)orth or (S)outh.")
+        elif start_y == 3:
+            valid_direction = "ws"
+            print("You can travel: (S)outh or (W)est.")
 
     direction = input("Direction: ")
     direction = direction.lower()
 
     if direction in valid_direction:
         if direction == "n":
-            start += 0.1
+            start_y += 1
         elif direction == "e":
-            start += 1
+            start_x += 1
         elif direction == "s":
-            start -= 0.1
+            start_y -= 1
         elif direction == "w":
-            start -= 1
-        start = round(start, 1)
+            start_x -= 1
     else:
         print("Not a valid direction!")
         direction = input("Direction: ")
         direction = direction.lower()
         if direction in valid_direction:
             if direction == "n":
-                start += 0.1
+                start_y += 1
             elif direction == "e":
-                start += 1
+                start_x += 1
             elif direction == "s":
-                start -= 0.1
+                start_y -= 1
             elif direction == "w":
-                start -= 1
-        start = round(start, 1)
+                start_x -= 1
 else:
     print("Victory!")
-
