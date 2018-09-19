7a837805cff1f9a799bcb1ae361a43fee409777b
Sandra <sandraa18@ru.is>
2018-09-19 20:32:28 +0000

    https://github.com/sandraa18/TileTraveller.git

--- implementation1.py
+++ implementation1.py
@@ -4,11 +4,24 @@
 #Finna út nýja gildi á start miðað við hvaða átt er slegin inn
 #Halda áfram í while lykkju þangað til við erum komin á reit 3.1, þá er búið að vinna
 
-start_x = 1
-start_y = 1
+#1. Which implementation was easier and why?
+    #
+#2. Which implementation is more readable and why?
+    #Seinna forritið, það með föllunum. Þá þarf ekki endalaust að skrifa það sama aftur og aftur.
+    # Aðal kóðinn verður styttri og ef föllin eru með nógu lýsandi nöfnum þá ætti að vera skiljanlegt hvað er í gangi í hverju falli
+#3. Which problems in the first implementations were you able to solve with the latter implementation?
 
-while start_x != 3 or start_y != 1 :
-    if start_x == 1:
+def x_and_y(x,y):
+    ''' Upphafsgildi á x og y '''
+    return x,y
+
+def Get_direction():
+    a = input("Direction: ")
+    a = a.lower()
+    return a
+
+def Get_valid_direction(a):
+    if a == 1:
         if start_y == 1:
             valid_direction = "n"
             print("You can travel: (N)orth.")
@@ -17,8 +30,8 @@ while start_x != 3 or start_y != 1 :
             print("You can travel: (N)orth or (E)ast or (S)outh.")
         elif start_y == 3:
             valid_direction = "es"
-            print("You can travel: (E)ast or (S)outh.")
-    elif start_x == 2:
+            print("You can travel: (E)ast or (S)outh.")   
+    elif a == 2:
         if start_y == 1:
             valid_direction = "n"
             print("You can travel: (N)orth.")
@@ -28,7 +41,7 @@ while start_x != 3 or start_y != 1 :
         elif start_y == 3:
             valid_direction = "ew"
             print("You can travel: (E)ast or (W)est.")
-    elif start_x == 3 :
+    elif a == 3:
         if start_y == 1:
             valid_direction = "n"
             print("You can travel: (N)orth.")
@@ -38,31 +51,38 @@ while start_x != 3 or start_y != 1 :
         elif start_y == 3:
             valid_direction = "ws"
             print("You can travel: (S)outh or (W)est.")
+    return valid_direction
 
-    direction = input("Direction: ")
-    direction = direction.lower()
+def Get_new_x_and_y (d, x, y):
+    if d == "n":
+        y += 1
+    elif d == "e":
+        x += 1
+    elif d == "s":
+        y -= 1
+    elif d == "w":
+        x -= 1    
+    return x, y
 
-    if direction in valid_direction:
-        if direction == "n":
-            start_y += 1
-        elif direction == "e":
-            start_x += 1
-        elif direction == "s":
-            start_y -= 1
-        elif direction == "w":
-            start_x -= 1
+def If_valid_direction(d, v, x, y):
+    if d in v:
+        x, y = Get_new_x_and_y(direction, start_x, start_y)    
     else:
-        print("Not a valid direction!")
-        direction = input("Direction: ")
-        direction = direction.lower()
-        if direction in valid_direction:
-            if direction == "n":
-                start_y += 1
-            elif direction == "e":
-                start_x += 1
-            elif direction == "s":
-                start_y -= 1
-            elif direction == "w":
-                start_x -= 1
+        print ("Not a valid direction!")
+        d = Get_direction()
+        x, y = Get_new_x_and_y(direction, start_x, start_y)
+    return x, y
+
+    
+
+start_x, start_y = x_and_y(1,1)
+
+while start_x != 3 or start_y != 1 :
+   
+    valid_direction = Get_valid_direction(start_x)
+
+    direction = Get_direction()
+
+    start_x, start_y = If_valid_direction(direction, valid_direction, start_x, start_y)
 else:
     print("Victory!")
